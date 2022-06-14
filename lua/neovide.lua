fd7eaec139c838e5a666790509a7bb5c69c57603
floaterest <56704092+floaterest@users.noreply.github.com>
2022-06-14 08:37:14 -0400

    feat(neovide) no animation

--- lua/neovide.lua
+++ lua/neovide.lua
@@ -1,6 +1,7 @@
 vim.o.gfn = 'Iosevka Custom NF,UD デジタル 教科書体 N-R:h16'
 
 vim.g.neovide_remember_window_size = true
+vim.g.neovide_cursor_animation_length = 0
 
 -- increase/decease font size
 function _G.incfont(amount)
