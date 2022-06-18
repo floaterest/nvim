dbc0d3070a4fee0c9d7ef1a0734c2df1e38481dc
floaterest <56704092+floaterest@users.noreply.github.com>
2022-06-18 09:34:13 -0400

    feat(o) more options

--- lua/core/o.lua
+++ lua/core/o.lua
@@ -1,39 +1,45 @@
+local o = vim.o
+
 -- allow backspace
-vim.o.bs = 'indent,eol,start'
+o.bs = 'indent,eol,start'
 -- timeout in ms (e.g. for whichkey)
-vim.o.tm = 500
+o.tm = 500
 -- num of lines to keep above/below cursor
-vim.o.so = 8
--- shift width
-vim.o.sw = 4
--- tabstop
-vim.o.ts = 4
+o.so = 8
+-- columns per shift
+o.sw = 4
+-- spaces per tab
+o.sts = 4
+-- columns per tabstop
+o.ts = 4
+-- <tab> expands to spaces
+o.et = true
 
 -- rel + abs line number
-vim.o.nu = true
-vim.o.rnu = true
+o.nu = true
+o.rnu = true
 -- keep visual indent on wrap
-vim.o.bri = true
+o.bri = true
 -- complete option
-vim.o.cot = "menuone,noinsert,noselect"
+o.cot = "menuone,noinsert,noselect"
 -- allow multiple unsaved buffers
-vim.o.hid = true
+o.hid = true
 -- no highlight after search
-vim.o.hls = false
+o.hls = false
 -- don't syntax highlight after 1000 lines
-vim.o.smc = 1000
+o.smc = 1000
 -- don't show '-- insert --'
-vim.o.smd = false
+o.smd = false
 -- no swap file
-vim.o.swf = false
+o.swf = false
 -- use 24-bit color
-vim.o.tgc = true
+o.tgc = true
 
 -- conceal level
-vim.o.cole = 2
+o.cole = 2
 -- no word wrap
-vim.o.wrap = false
+o.wrap = false
 -- enable mouse
-vim.o.mouse = "a"
+o.mouse = "a"
 -- highlight all search results
-vim.o.hlsearch = true
+o.hlsearch = true
