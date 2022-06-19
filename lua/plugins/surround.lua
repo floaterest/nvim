603758f4c0692c42813de991d8446019879dd1de
floaterest <56704092+floaterest@users.noreply.github.com>
2022-06-19 19:38:26 -0400

    feat(surround) not map insert mode

--- lua/plugins/surround.lua
+++ lua/plugins/surround.lua
@@ -2,6 +2,7 @@ require('surround').setup({
     mappings_style = 'surround',
     prefix = 's',
 	prompt = false,
+	map_insert_mode = false,
     pairs = {
         nestable = {
 			p = { '(', ')' }, b = { '[', ']' }, B = { '{', '}' }, 
