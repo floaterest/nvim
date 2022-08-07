81d020674301e411307ea5a4013a3bdc7cb07c86
floaterest <56704092+floaterest@users.noreply.github.com>
2022-08-06 20:28:05 -0400

    refactor(plugin) remove ununsed

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,43 +1,39 @@
 local fn = vim.fn
 local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
-    packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    packer_bootstrap = fn.system({
+        'git', 'clone', '--depth', '1', 
+        'https://github.com/wbthomason/packer.nvim', install_path
+    })
 end
 
 require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
 
-    -- use { 'Shougo/deoplete.nvim', run = ':UpdateRemotePlugins' }
-    -- use 'beeender/Comrade'
-    -- use { 'neoclide/coc.nvim', branch = 'release' }
-
-    use { 'nvim-lualine/lualine.nvim', requires = { 'kyazdani42/nvim-web-devicons' } }
-    use { 'nvim-telescope/telescope.nvim', requires = { 'nvim-lua/plenary.nvim' } }
-    use 'vim-pandoc/vim-pandoc-syntax'
-    use 'lukas-reineke/indent-blankline.nvim'
-    use 'nvim-treesitter/nvim-treesitter'
-    -- use 'kyazdani42/nvim-tree.lua'
+    use 'windwp/nvim-autopairs'
 	use 'akinsho/bufferline.nvim'
     use 'numToStr/Comment.nvim'
-    use 'windwp/nvim-autopairs'
-    use 'ur4ltz/surround.nvim'
-	use 'folke/which-key.nvim'
+    use 'lukas-reineke/indent-blankline.nvim'
+    use { 'nvim-lualine/lualine.nvim', requires = { 'kyazdani42/nvim-web-devicons' } }
     use 'L3MON4D3/LuaSnip'
+    use 'vim-pandoc/vim-pandoc-syntax'
+    use 'ur4ltz/surround.nvim'
+    use { 'nvim-telescope/telescope.nvim', requires = { 'nvim-lua/plenary.nvim' } }
+    use 'nvim-treesitter/nvim-treesitter'
+	use 'folke/which-key.nvim'
 
     if packer_bootstrap then require('packer').sync() end
 end)
 
 -- plugin setups
 require('plugins.autopairs')
-require('plugins.indentline')
-require('plugins.treesitter')
-require('plugins.lualine')
-require('plugins.whichkey')
 require('plugins.bufferline')
-require('plugins.telescope')
--- require('plugins.nvimtree')
 require('plugins.comment')
+require('plugins.indentline')
+require('plugins.lualine')
 require('plugins.luasnip')
-require('plugins.surround')
 require('plugins.pandoc')
-
+require('plugins.surround')
+require('plugins.telescope')
+require('plugins.treesitter')
+require('plugins.whichkey')
