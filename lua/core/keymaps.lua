cde7fcf18fb166125acd4e2b5444467e9d2f4b22
floaterest <56704092+floaterest@users.noreply.github.com>
2022-11-19 21:19:48 -0500

    refactor(cmp) move keymaps to `keymaps.lua`

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -10,13 +10,19 @@ local function noremap(mode, lhs, rhs, opts)
     vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
--- keymaps that don't depend on plugins
+
+local function has_words_before()
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
 
 function M.map(t)
     vim.tbl_map(function(v) noremap(unpack(v)) end, t)
 end
 
 
+-- keymaps that don't depend on plugins
 function M.vanilla()
     M.map({
         -- delete word
@@ -72,6 +78,36 @@ M.which = {
     },
 }
 
-
+-- nvim-cmp
+function M.cmp(cmp, luasnip)
+    return {
+        -- tab to luasnip expand or cmp complete
+        ['<tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+            elseif has_words_before() then
+                cmp.complete()
+            else
+                fallback()
+            end
+        end, { 'i', 's' }),
+         ['<s-tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+            else
+                fallback()
+            end
+        end, { 'i', 's' }),
+        ['<c-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<c-f>'] = cmp.mapping.scroll_docs(4),
+        ['<c-space>'] = cmp.mapping.complete(),
+        ['<c-e>'] = cmp.mapping.abort(),
+        ['<cr>'] = cmp.mapping.confirm({ select = true }),
+    }
+end
 
 return M
