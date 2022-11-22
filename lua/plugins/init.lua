7f03d763ff802b4392da59808b39ff1cb31bdf5d
floaterest <56704092+floaterest@users.noreply.github.com>
2022-11-21 20:23:18 -0500

    refactor(cmp) remove unused sources

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -14,14 +14,11 @@ require('packer').startup(function(use)
     use 'williamboman/mason-lspconfig.nvim'
     use 'neovim/nvim-lspconfig'
 
-    use "hrsh7th/nvim-cmp"
-    use "saadparwaiz1/cmp_luasnip"
-    use "hrsh7th/cmp-buffer"
-    use "hrsh7th/cmp-calc"
-    use "hrsh7th/cmp-cmdline"
-    use "hrsh7th/cmp-nvim-lsp"
-    use "hrsh7th/cmp-nvim-lua"
-    use "hrsh7th/cmp-path"
+    use 'hrsh7th/nvim-cmp'
+    use 'saadparwaiz1/cmp_luasnip'
+    use 'hrsh7th/cmp-buffer'
+    use 'hrsh7th/cmp-nvim-lsp'
+    use 'hrsh7th/cmp-nvim-lua'
 
     use 'windwp/nvim-autopairs'
 	use 'akinsho/bufferline.nvim'
