c71a684063109ed535ac0741df40e4cffac4c964
floaterest <56704092+floaterest@users.noreply.github.com>
2022-11-21 20:04:49 -0500

    feat(cmp) enable when not in comment

--- lua/plugins/behavior/cmp.lua
+++ lua/plugins/behavior/cmp.lua
@@ -2,7 +2,19 @@ local cmp = require('cmp')
 local luasnip = require('luasnip')
 local keymaps = require('core.keymaps').cmp(cmp, luasnip)
 
+local function notcomment()
+    local context = require('cmp.config.context')
+    -- if in command mode
+    if vim.api.nvim_get_mode().mode == 'c' then
+        return true
+    end
+    local treesitter = context.in_treesitter_capture("comment")
+    local syntax = context.in_syntax_group("Comment")
+    return not treesitter and not syntax
+end
+
 cmp.setup({
+    enabled = notcomment,
     snippet = {
         expand = function(args) luasnip.lsp_expand(args.body) end,
     },
