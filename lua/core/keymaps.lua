0d0abce0975913b409d40b8a27c9c3446bd82ba3
floaterest <56704092+floaterest@users.noreply.github.com>
2022-11-22 19:10:44 -0500

    refactor(keymaps) diagnostics

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -2,21 +2,22 @@
 
 local M = {}
 
+local options = { silent = true, noremap = true }
+
 local function has_words_before()
     local line, col = unpack(vim.api.nvim_win_get_cursor(0))
     return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
 -- silent noremap
--- (not snore map)
+-- (not snore map because it's silent)
 local function snoremap(t)
     vim.tbl_map(function(v)
-        local mode, lhs, rhs, opts = unpack(v)
-        local options = { silent = true, noremap = true }
+        local mode, key, action, opts = unpack(v)
         if opts then
             options = vim.tbl_extend('force', options, opts)
         end
-        vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+        vim.keymap.set(mode, key, action, options)
     end, t)
 end
 
@@ -73,6 +74,11 @@ function M.vanilla()
         { 'n', 'Y', 'v$hy' },
         -- no ex-cammand
         { 'n', 'Q', '' },
+
+        { 'n', '<leader>e', vim.diagnostic.open_float },
+        { 'n', '[d', vim.diagnostic.goto_prev },
+        { 'n', ']d', vim.diagnostic.goto_next },
+        { 'n', '<leader>q', vim.diagnostic.setloclist },
     })
 end
 
