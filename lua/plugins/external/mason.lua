825b5f885912ae5dcdf82b7aef17a4ac86c6ea91
floaterest <56704092+floaterest@users.noreply.github.com>
2022-11-22 19:10:44 -0500

    refactor(keymaps) diagnostics

--- lua/plugins/external/mason.lua
+++ lua/plugins/external/mason.lua
@@ -2,12 +2,6 @@ local servers = {
     'tsserver', 'pyright'
 }
 
-local opts = { noremap=true, silent=true }
-vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
-
 local on_attach = function(client, bufnr)
     -- Enable completion triggered by <c-x><c-o>
     vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
