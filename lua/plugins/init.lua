624e6edde8455e06e91c11dfe034e039ea119cb5
floaterest <56704092+floaterest@users.noreply.github.com>
2022-11-23 20:24:26 -0500

    refactor(plugins) local vars in init

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,11 +1,9 @@
 local fn = vim.fn
 local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-if fn.empty(fn.glob(install_path)) > 0 then
-    packer_bootstrap = fn.system({
+local packer_bootstrap = fn.empty(fn.glob(install_path)) > 0 and fn.system({
         'git', 'clone', '--depth', '1',
         'https://github.com/wbthomason/packer.nvim', install_path
-    })
-end
+    }) or false
 
 require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
