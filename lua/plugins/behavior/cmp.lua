c3e3c3c15c35c3d53eb8248fca421f582f437811
floaterest <56704092+floaterest@users.noreply.github.com>
2022-11-24 21:13:18 -0500

    refactor(cmp, mason) more FP

--- lua/plugins/behavior/cmp.lua
+++ lua/plugins/behavior/cmp.lua
@@ -26,14 +26,3 @@ cmp.setup({
         { name = 'buffer' },
     })
 })
-
-
--- Setup lspconfig.
-local capabilities = require('cmp_nvim_lsp').update_capabilities(
-    vim.lsp.protocol.make_client_capabilities()
-)
-
--- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-require('lspconfig')['pyright'].setup {
-    capabilities = capabilities
-}
