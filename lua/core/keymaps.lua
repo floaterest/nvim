5175be6231895c4c3d48732629c92afada5ca25b
floaterest <56704092+floaterest@users.noreply.github.com>
2022-12-09 16:39:38 -0500

    refactor(comment) more keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -63,11 +63,30 @@ end
 
 -- comment
 function M.comment()
+    -- these are default mappings
     return {
-        ---Line-comment toggle keymap
-        line = 'gcc',
-        ---Block-comment toggle keymap
-        block = 'gbc',
+        toggler = {
+            ---Line-comment toggle keymap
+            line = 'gcc',
+            ---Block-comment toggle keymap
+            block = 'gbc',
+        },
+        ---LHS of operator-pending mappings in NORMAL and VISUAL mode
+        opleader = {
+            ---Line-comment keymap
+            line = 'gc',
+            ---Block-comment keymap
+            block = 'gb',
+        },
+        ---LHS of extra mappings
+        extra = {
+            ---Add comment on the line above
+            above = 'gcO',
+            ---Add comment on the line below
+            below = 'gco',
+            ---Add comment at the end of line
+            eol = 'gcA',
+        },
     }
 end
 
