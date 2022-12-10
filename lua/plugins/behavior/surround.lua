a0998fbc479ef0df377cd6fb5c1ced14399671a2
floaterest <56704092+floaterest@users.noreply.github.com>
2022-12-10 16:49:51 -0500

    feat(surround) use `kylechui/nvim-surround`

--- lua/plugins/behavior/surround.lua
+++ lua/plugins/behavior/surround.lua
@@ -1,16 +1,1 @@
-require('surround').setup({
-    mappings_style = 'surround',
-    prefix = 's',
-	prompt = false,
-	map_insert_mode = false,
-    pairs = {
-        nestable = {
-			p = { '(', ')' }, b = { '[', ']' }, B = { '{', '}' },
-			a = { '<', '>' }, s = { '*', '*' },
-		},
-        linear = {
-			q = { "'", "'" }, t = { '`', '`' }, d = { '"', '"' },
-			m = { '$', '$' }, M = { '$$', '$$' }
-		},
-    },
-})
+require('nvim-surround').setup({})
