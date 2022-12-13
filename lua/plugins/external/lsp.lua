a48e32a8d7d581a4a72f0e23bd0cfc04ffda3fd9
floaterest <56704092+floaterest@users.noreply.github.com>
2022-12-13 16:12:17 -0500

    feat(lsp) support server-specific options

--- lua/plugins/external/lsp.lua
+++ lua/plugins/external/lsp.lua
@@ -6,13 +6,17 @@ local servers = {
     'pyright',
 }
 
+local options = {
+    -- will add server-specific options later
+}
+
 local capabilities = require('cmp_nvim_lsp').update_capabilities(
     vim.lsp.protocol.make_client_capabilities()
 )
 
 vim.tbl_map(function(server)
-    lsconfig[server].setup({
+    lsconfig[server].setup(vim.tbl_extend('force', {
         on_attach = on_attach,
         capabilities = capabilities,
-    })
+    }, options[server] or {}))
 end, servers)
