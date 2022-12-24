93e467f3d418f33f4f4dc1eaf62ccb02eca0b00c
floaterest <56704092+floaterest@users.noreply.github.com>
2022-12-24 18:38:18 -0500

    feat(neovide) new settings

--- lua/neovide.lua
+++ lua/neovide.lua
@@ -1,6 +1,6 @@
-vim.o.gfn = 'Iosevka Custom NF,UD デジタル 教科書体 N-R:h16'
+vim.o.gfn = 'Iosevka Custom NF,UD デジタル 教科書体 N-R:h24'
 
-vim.g.neovide_remember_window_size = true
+-- vim.g.neovide_remember_window_size = true
 vim.g.neovide_cursor_animation_length = 0
 
 -- increase/decease font size
