201e696726ba95bb532887bca073ce727dacdb97
floaterest <56704092+floaterest@users.noreply.github.com>
2022-12-28 20:28:15 -0500

    feat(luasnip) lua if

--- luasnippets/lua.lua
+++ luasnippets/lua.lua
@@ -1,6 +1,8 @@
-return {
-    s('f', fmta('function <>\nend', { i(0) })),
-    s('lf', fmta('local function <>\nend', { i(0) })),
-}, {
+local i0 = { i(0) }
 
+return {
+    s('f', fmta('function <>\nend', i0)),
+    s('lf', fmta('local function <>\nend', i0)),
+}, {
+    s('if ', fmta('if <> then\n\t<>\nend', { i(1), i(0) }))
 }
