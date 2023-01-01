26e8164c38f5e7d3fe623d66a0a110ff653a39ff
floaterest <56704092+floaterest@users.noreply.github.com>
2023-01-01 16:43:35 -0500

    format(lualine) foreshadowing

--- lua/plugins/interface/lualine.lua
+++ lua/plugins/interface/lualine.lua
@@ -62,13 +62,5 @@ lualine.setup({
         lualine_y = {},
         lualine_z = {}
     },
-    -- tabline = {
-        -- lualine_a = {'buffers'},
-        -- lualine_b = {},
-        -- lualine_c = {},
-        -- lualine_x = {},
-        -- lualine_y = {},
-        -- lualine_z = {'tabs'}
-    -- },
-    extensions = { 'nvim-tree' }
+    extensions = { 'nvim-tree', --[[ 'nvim-dap-ui' ]] }
 })
