714196e3716f9c78834fe240f4c26ba84548ad47
floaterest <56704092+floaterest@users.noreply.github.com>
2023-01-17 20:15:24 -0500

    refactor(dapui, dui) rename ui to dui

--- lua/plugins/external/dapui.lua
+++ lua/plugins/external/dapui.lua
@@ -1,12 +1,12 @@
 local dap = require("dap")
 local dapui = require("dapui")
-local ui = require("plugins.external.ui")
+local dui = require("plugins.external.dui")
 
 return function(register, keymaps)
-    dapui.setup({ layouts = ui.layouts() })
+    dapui.setup({ layouts = dui.layouts() })
     dap.listeners.after.event_initialized["dapui_config"] = dapui.open
     dap.listeners.before.event_terminated["dapui_config"] = dapui.close
     dap.listeners.before.event_exited["dapui_config"] = dapui.close
-    register(keymaps, ui)
+    register(keymaps, dui)
 end
 
