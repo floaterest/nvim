dbb037a55fb7a19cf62acaf69939a1161f1b92a0
floaterest <56704092+floaterest@users.noreply.github.com>
2023-01-17 19:51:19 -0500

    feat(dapui, keymaps) toggle each ui element

--- lua/plugins/external/dapui.lua
+++ lua/plugins/external/dapui.lua
@@ -1,9 +1,12 @@
 local dap = require("dap")
 local dapui = require("dapui")
-local layouts = require("plugins.external.ui").layouts()
+local ui = require("plugins.external.ui")
 
-dapui.setup({ layouts = layouts })
-dap.listeners.after.event_initialized["dapui_config"] = dapui.open
-dap.listeners.before.event_terminated["dapui_config"] = dapui.close
-dap.listeners.before.event_exited["dapui_config"] = dapui.close
+return function(register, keymaps)
+    dapui.setup({ layouts = ui.layouts() })
+    dap.listeners.after.event_initialized["dapui_config"] = dapui.open
+    dap.listeners.before.event_terminated["dapui_config"] = dapui.close
+    dap.listeners.before.event_exited["dapui_config"] = dapui.close
+    register(keymaps, ui)
+end
 
