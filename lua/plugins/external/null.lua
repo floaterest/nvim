102f5786a881cd762df9f400d4c259e9639c271e
floaterest <56704092+floaterest@users.noreply.github.com>
2023-02-02 18:14:51 -0500

    feat: eslint on svelte

--- lua/plugins/external/null.lua
+++ lua/plugins/external/null.lua
@@ -3,9 +3,16 @@ local null = require('null-ls')
 local ca = null.builtins.code_actions
 local fo = null.builtins.formatting
 
+local eslint = {
+    filetypes =  {
+        'javascript', 'javascriptreact', 'typescript', 'typescriptreact',
+        'svelte',
+    }
+}
+
 local sources = {
-    ca.eslint_d,
-    fo.eslint_d,
+    ca.eslint_d.with(eslint),
+    fo.eslint_d.with(eslint),
     fo.blue, -- I'll use black when it supports single quotes
     fo.stylua,
 }
