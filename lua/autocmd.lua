7c0a5c66c276908cc31d2ea84214dcbffc1a6f1b
floaterest <floaterest@users.noreply.github.com>
2023-02-06 18:44:32 -0500

    fix(autocmd) autosave

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -1,15 +1,16 @@
-local groups = { custom = {
+local commands = {
     FocusGained = function() vim.cmd('checktime') end,
-    [{ 'FocusLost', 'BufLeave' }] = function()
-        if vim.bo.bt ~= '' then return end
-        vim.cmd('w')
+    [{ 'FocusLost', 'BufLeave' }] = function(args)
+        -- modified file with no buftype
+        if vim.fn.getbufinfo(args.buf)[1].changed == 1 and vim.bo.bt == '' then
+            vim.cmd.w()
+        end
     end
-} }
-
-for group, commands in pairs(groups) do
-    vim.api.nvim_create_augroup(group, { clear = true })
-    for events, callback in pairs(commands) do
-        local opts = { group = group, callback = callback }
-        vim.api.nvim_create_autocmd(events, opts)
-    end
+}
+ 
+vim.api.nvim_create_augroup('custom', { clear = true })
+for events, callback in pairs(commands) do
+    local opts = { group = group, callback = callback }
+    -- https://neovim.io/doc/user/api.html#api-autocmd
+    vim.api.nvim_create_autocmd(events, opts)
 end
