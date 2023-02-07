ba79598bebb187f8ae4735ff7a1c3556201a4d99
floaterest <floaterest@users.noreply.github.com>
2023-02-07 18:42:16 -0500

    refactor: use custom env

--- lua/plugins/behavior/luasnip.lua
+++ lua/plugins/behavior/luasnip.lua
@@ -3,42 +3,6 @@ local luasnip = require('luasnip')
 luasnip.config.set_config({
     history = false, enable_autosnippets = true,
     region_check_events = 'InsertEnter', store_selection_keys = '<tab>',
-    snip_env = {
-        s = luasnip.s, sn = luasnip.sn, t = luasnip.t, i = luasnip.i,
-        f = require('luasnip.nodes.functionNode').F,
-        d = require('luasnip.nodes.dynamicNode').D,
-        l = require('luasnip.extras').lambda,
-        rep = require('luasnip.extras').rep,
-        fmt = require('luasnip.extras.fmt').fmt,
-        fmta = require('luasnip.extras.fmt').fmta,
-        parse = require('luasnip.util.parser').parse_snippet,
-        pack = function(snippets)
-            local t = {}
-            for _, v in ipairs(snippets) do
-                if type(v) == 'table' then
-                    for _, w in ipairs(v) do
-                        table.insert(t, w)
-                    end
-                else
-                    table.insert(t, v)
-                end
-            end
-            return t
-        end,
-        map = function(tt, f)
-            local t = {}
-            for k, v in pairs(tt) do
-                table.insert(t, f(k, v))
-            end
-            return t
-        end,
-        trig = function(tr)
-            return { trig = tr, wordTrig = false }
-        end,
-        rtrig = function(tr)
-            return { trig = tr, wordTrig = false, regTrig = true }
-        end
-    }
 })
 
 luasnip.filetype_extend('svelte', { 'typescript' })
--- luasnippets/svelte.lua
+++ luasnippets/svelte.lua
@@ -1,3 +1,5 @@
+require('util.luasnip')()
+
 local snips = {
     s('ts', fmt('<script lang="ts">\n\t{}\n</script>', { i(0) })),
     s('sass', fmt('<style lang="sass">\n\t{}\n</style>', { i(0) })),
