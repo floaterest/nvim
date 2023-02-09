99e9587a47ad4f21e16252d0d3a40b8795993849
floaterest <floaterest@users.noreply.github.com>
2023-02-09 18:57:12 -0500

    refactor(snip/lua)

--- luasnippets/lua.lua
+++ luasnippets/lua.lua
@@ -4,7 +4,7 @@ local snips = {
     { 'fu', 'function <>\nend' },
     { 'lf', 'local function <>\nend' },
     { 'if', 'if <> then\n\t<>\nend' },
-    { 'for', 'for <> in pairs(<>) do\n\t<>\nend' }
+    { 'for', 'for <> in pairs(<>) do\n\t<>\nend' },
 }
 
 return ifmtas(snips)
