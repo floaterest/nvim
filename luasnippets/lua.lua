918e41734be3ad3208da60f172d79a66e5bb54b8
floaterest <floaterest@users.noreply.github.com>
2023-02-13 18:22:17 -0500

    fix(snip/lua) keywords

--- luasnippets/lua.lua
+++ luasnippets/lua.lua
@@ -3,8 +3,8 @@ require('util.luasnip')
 local snips = {
     fu = 'function <>\nend',
     lf = 'local function <>\nend',
-    if = 'if <> then\n\t<>\nend',
-    for = 'for <> in pairs(<>) do\n\t<>\nend',
+    ['if'] = 'if <> then\n\t<>\nend',
+    ['for'] = 'for <> in pairs(<>) do\n\t<>\nend',
 }
 
 return ifmtas(snips)
