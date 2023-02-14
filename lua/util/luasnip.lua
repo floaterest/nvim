50b5a673fd301daadc9dc8b6629b150f53517459
floaterest <floaterest@users.noreply.github.com>
2023-02-13 19:01:48 -0500

    refactor(luasnip)

--- lua/util/luasnip.lua
+++ lua/util/luasnip.lua
@@ -2,6 +2,7 @@ local luasnip = require('luasnip')
 local func = require('plenary.functional')
 
 s = luasnip.s
+sn = luasnip.snippet_node
 t = luasnip.t
 i = luasnip.i
 f = require('luasnip.nodes.functionNode').F
@@ -10,40 +11,12 @@ l = require('luasnip.extras').lambda
 rep = require('luasnip.extras').rep
 fmt = require('luasnip.extras.fmt').fmt
 fmta = require('luasnip.extras.fmt').fmta
-trig = function(tr)
-    return { trig = tr, wordTrig = false }
-end
-rtrig = function(tr)
-    return { trig = tr, wordTrig = false, regTrig = true }
-end
 
-function pack(snippets)
-    local t = {}
-    for _, v in ipairs(snippets) do
-        if type(v) == 'table' then
-            for _, w in ipairs(v) do
-                table.insert(t, w)
-            end
-        else
-            table.insert(t, v)
-        end
-    end
-    return t
-end
-
-function map(tt, f)
-    local t = {}
-    for k, v in pairs(tt) do
-        table.insert(t, f(k, v))
-    end
-    return t
-end
-
-function starmap(fun, t)
+function kv_map(fun, t)
     return func.kv_map(function(kv) return fun(unpack(kv)) end, t)
 end
 
-ifmtas = func.partial(starmap, function(trigger, template)
+ifmtas = func.partial(kv_map, function(trigger, template)
     -- return snippet with each delimiter replaced by insert node
     local _, n = template:gsub('<>', '')
     local option = {}
