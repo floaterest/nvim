fd5518990713d8e3374d758cc88dcf050dbd2892
floaterest <floaterest@users.noreply.github.com>
2023-09-10 19:39:21 -0400

    feat(lsp) haskell

--- lua/plugins/external/lsp.lua
+++ lua/plugins/external/lsp.lua
@@ -13,18 +13,25 @@ local rust = { ['rust-analyzer']= {
     },
 } }
 
+local haskell = {
+    formattingProvider = "fourmolu",
+}
+
 local servers = {
     tsserver = {},
     pyright = {},
     svelte = {},
     clangd = {},
+    hls = {
+        filetypes = { 'haskell', 'lhaskell', 'cabal' },
+        haskell = haskell,
+    },
     rust_analyzer = { settings = rust },
 }
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 return function(register, attach)
     local onatt = function(client, buffer) register(attach, client, buffer) end
-    local opts = { on_attach = onatt, capabilities = capabilities }
+    local opts = { on_attach = onatt }
     func.kv_map(function(kv)
         local server, options = unpack(kv)
         lsconfig[server].setup(vim.tbl_extend('error', opts, options))
