f6c04a935a063a784b365377acef1aa083ffdebe
floaterest <floaterest@users.noreply.github.com>
2023-09-11 19:14:26 -0400

    feat(lsp) check if binary exists

--- lua/plugins/external/lsp.lua
+++ lua/plugins/external/lsp.lua
@@ -29,11 +29,18 @@ local servers = {
     rust_analyzer = { settings = rust },
 }
 
+local function exists(config)
+    local binary = config.document_config.default_config.cmd[1]
+    return vim.fn.executable(binary) == 1
+end
+
 return function(register, attach)
     local onatt = function(client, buffer) register(attach, client, buffer) end
     local opts = { on_attach = onatt }
     func.kv_map(function(kv)
         local server, options = unpack(kv)
-        lsconfig[server].setup(vim.tbl_extend('error', opts, options))
+        if exists(lsconfig[server]) then
+            return lsconfig[server].setup(vim.tbl_extend('error', opts, options))
+        end
     end, servers)
 end
