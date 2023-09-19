3e3864d8e212384bb9e26d7273761ab198d93811
floaterest <floaterest@users.noreply.github.com>
2023-09-19 18:28:41 -0400

    feat(snippets) move markdown math to tex

--- luasnippets/tex.lua
+++ luasnippets/tex.lua
@@ -2,28 +2,70 @@ local List = require('plenary.collections.py_list')
 require('util.luasnip')
 
 local commands = {
+    -- logic
+    L = '\\mathcal L_p',
+    Ls = '\\mathcal L_p^s',
+    v = '\\vdash',
+    V = '\\vDash',
+    B = '\\mathcal B',
+    F = '\\{F,T\\}',
+    oo = { '\\overline', '{\\overline ', '}' },
+    En = '\\mathcal E_\\neg',
+    Es = '\\mathcal E_\\square',
+    W = '\\mathcal W_p',
+    -- computation
+    Sa = '\\Sigma^\\ast',
+    Ga = '\\Gamma^\\ast',
     an = { '', '\\langle ', '\\rangle' },
-    s = '\\sigma',
+    fl = { '', '\\lfloor', '\\rfloor' },
+    ce = { '', '\\lceil', '\\rceil' },
+    -- proof
+    tf = '\\therefore',
+    bc = '\\because',
+    tF = '\\therefore&&',
+    bC = '\\because&&',
+    tff = '\\therefore\\,&',
+    bcc = '\\because\\,&',
+    qed = '\\quad\\square',
+    f = { '\\frac', '{', '}' },
+    im = '\\implies',
+    i = '^{-1}',
+    fo = '\\forall',
+    ex = '\\exists',
+    -- typography
+    q = '\\quad',
+    en = '\\enspace',
+    st = '\\text{ st }',
+    t = { '\\text', '{', '}' },
+    tt = { '\\texttt', '{', '}' },
+    bf = { '\\textbf', '{', '}' },
+    rm = { '\\mathrm', '{', '}' },
+    g = { '\\tag', '{', '}' },
+    ob = { '\\overbrace', '{', '}' },
+    ub = { '\\underbrace', '{', '}' },
+    ol = '\\overline',
+    ul = '\\underline',
     ss = { '\\subsection*', '{', '}' },
     sss = { '\\subsubsection*', '{', '}' },
+    -- set
     no = '\\varnothing',
-
-    p = '\\varphi',
-    im = '\\implies',
-    bf = { '\\textbf', '{', '}' },
+    un = '\\cup',
+    Un = '\\bigcup',
+    mi = '\\setminus',
     se = '\\subseteq',
-    a = '^\\ast',
     su = '\\subset',
+    -- greek
+    a = '\\alpha',
+    b = '\\beta',
     d = '\\delta',
-    l = '\\lambda',
-    qed = '\\enspace\\square',
+    D = '\\Delta',
     e = '\\varepsilon',
-    S = '\\Sigma', G = '\\Gamma', D = '\\Delta', P = '\\Phi',
-    q = '\\quad', en = '\\enspace',
-    tf = '\\therefore', bc = '\\because',
-    fo = '\\forall', ex = '\\exists',
-    tt = { '\\texttt', '{', '}' },
-    t = { '\\text', '{', '}' },
+    G = '\\Gamma',
+    l = '\\lambda',
+    P = '\\Phi',
+    p = '\\varphi',
+    S = '\\Sigma',
+    s = '\\sigma',
 }
 
 
@@ -63,23 +105,29 @@ local autosnippets = List.new({
         ['uu '] = '$$\n<>\n$$',
         ['ud '] = '$\\displaystyle<>$',
     }),
-    kv_slead(function(env) return fmta('\\begin{<env>}\n<>\n\\end{<env>}', {
-        env = t(env), d(1, function(_, parent)
-            local raw = vim.tbl_map(function(_, line)
-                return tostring(line:gsub('^%$+', ''))
-            end, parent.env.SELECT_RAW)
-            return sn(nil, #raw > 0 and t(raw) or { t('\t'), i(1) })
-        end)
-    }) end, environments),
-    kv_slead(function(sub) return type(sub) == 'string' and t(sub) or {
-        t(sub[1]),
-        d(1, function(_, parent) return sn(nil, {
-            t(sub[2]),
-            (#parent.env.SELECT_RAW > 0) and t(parent.env.SELECT_RAW) or i(1),
-            t(sub[3])
-        }) end)
-    } end, commands)
+    kv_slead(function(env)
+        return fmta('\\begin{<env>}\n<>\n\\end{<env>}', {
+            env = t(env),
+            d(1, function(_, parent)
+                local raw = vim.tbl_map(function(_, line)
+                    return tostring(line:gsub('^%$+', ''))
+                end, parent.env.SELECT_RAW)
+                return sn(nil, #raw > 0 and t(raw) or { t('\t'), i(1) })
+            end)
+        })
+    end, environments),
+    kv_slead(function(sub)
+        return type(sub) == 'string' and t(sub) or {
+            t(sub[1]),
+            d(1, function(_, parent)
+                return sn(nil, {
+                    t(sub[2]),
+                    (#parent.env.SELECT_RAW > 0) and t(parent.env.SELECT_RAW) or i(1),
+                    t(sub[3])
+                })
+            end)
+        }
+    end, commands)
 )
 
 return {}, autosnippets
-
