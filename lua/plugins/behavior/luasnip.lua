bbd30ed7a465f315760745d85f141e98440c9350
floaterest <floaterest@users.noreply.github.com>
2023-09-22 19:54:39 -0400

    refactor(luasnip)

--- lua/plugins/behavior/luasnip.lua
+++ lua/plugins/behavior/luasnip.lua
@@ -1,21 +1,23 @@
 local luasnip = require('luasnip')
 
 luasnip.config.set_config({
-    history = false, enable_autosnippets = true,
-    region_check_events = 'InsertEnter', store_selection_keys = '<tab>',
+    history = false,
+    enable_autosnippets = true,
+    region_check_events = 'InsertEnter',
+    store_selection_keys = '<tab>',
 })
 
 luasnip.filetype_extend('svelte', { 'typescript' })
 luasnip.filetype_extend('markdown', { 'tex' })
 
 require('luasnip.loaders.from_lua').lazy_load()
--- local path = { paths = { vim.fn.getcwd() .. '/luasnippets/' }}
--- require('luasnip.loaders.from_lua').lazy_load(path)
--- require('luasnip.loaders.from_lua').lazy_load('./luasnippets')
 
 vim.api.nvim_create_autocmd('BufWritePost', {
     pattern = '*/luasnippets/*.lua',
     callback = function() require('luasnip.loaders.from_lua').lazy_load() end
 })
 
-vim.api.nvim_create_user_command('LuaSnipEdit', function() require('luasnip.loaders.from_lua').edit_snippet_files()end, {})
+vim.api.nvim_create_user_command('LuaSnipEdit',
+    function() require('luasnip.loaders.from_lua').edit_snippet_files() end,
+    {}
+)
