e9f5766c5caaca7eda7a9a2e8a6997cafecf1425
floaterest <floaterest@users.noreply.github.com>
2023-09-23 17:56:48 -0400

    feat(indentline) thinner char

--- lua/plugins/interface/indentline.lua
+++ lua/plugins/interface/indentline.lua
@@ -1,2 +1,2 @@
 -- see syntax.lua for IndentMarker
-require("ibl").setup({ indent = { highlight = { 'IndentMarker' } }})
+require("ibl").setup({ indent = { highlight = { 'IndentMarker' } , char = '│' }})
