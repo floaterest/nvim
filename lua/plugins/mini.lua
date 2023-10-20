dd30b779f4ad126fd73e34c1047db44aa333a7f0
floaterest <floaterest@users.noreply.github.com>
2023-10-20 18:09:43 -0400

    feat(plugins.mini) comment

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -2,3 +2,9 @@ require('mini.indentscope').setup({
     draw = { delay = 0 },
     symbol = '│',
 })
+
+require('mini.comment').setup({
+    options = {
+        ignore_blank_line = true,
+    }
+})
