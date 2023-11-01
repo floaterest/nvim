c8c05dd4fa05c1e68358e710176870bac9df9a53
floaterest <floaterest@users.noreply.github.com>
2023-10-31 20:26:24 -0400

    refactor(pluigns.alpha)

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -3,7 +3,6 @@ local with = context_manager.with
 local open = context_manager.open
 local path = vim.fn.stdpath('config') .. '/alpha/neovim.txt'
 
-
 local header = {
     type = 'text',
     val = {},
@@ -13,30 +12,28 @@ local header = {
     }
 }
 
-local function lines(reader)
+with(open(path, 'r'), function(reader)
     repeat
         local line = reader:read('*l')
         table.insert(header.val, line)
     until line == nil
-end
-with(open(path, 'r'), lines)
+end)
 
 local dashboard = require('alpha.themes.dashboard')
-local buttons = {
-    type = 'group',
-    val = {
-        dashboard.button('f', '  Find file',       '<cmd>Telescope find_files<cr>'),
-        dashboard.button('r', '  Recent files',    '<cmd>Telescope oldfiles<cr>'),
-        dashboard.button('g', '  Find text',       '<cmd>Telescope live_grep<cr>'),
-        dashboard.button('q', '  Quit',            '<cmd>qa<cr>'),
-    },
-    opts = { spacing = 1 },
-}
 require('alpha').setup({
     layout = {
         header,
         { type = 'padding', val = 1 },
-        buttons
+        {
+            type = 'group',
+            val = {
+                dashboard.button('f', '  Find file',       '<cmd>Telescope find_files<cr>'),
+                dashboard.button('r', '  Recent files',    '<cmd>Telescope oldfiles<cr>'),
+                dashboard.button('g', '  Find text',       '<cmd>Telescope live_grep<cr>'),
+                dashboard.button('q', '  Quit',            '<cmd>qa<cr>'),
+            },
+            opts = { spacing = 1 },
+        }
     },
     opts = { margin = 5 },
 })
