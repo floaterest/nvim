58caabdd506415fe9f8ee75d6f5a113de82881c5
floaterest <floaterest@users.noreply.github.com>
2023-11-01 17:56:26 -0400

    feat(g) filetype.lua

--- init.lua
+++ init.lua
@@ -6,4 +6,8 @@ require('plugins')
 -- see log in ~/.local/state/nvim/lsp.log
 -- vim.lsp.set_log_level("debug")
 
+-- enable filetype.lua
+vim.g.do_filetype_lua = true
+vim.g.did_load_filetypes = true
+
 if vim.g.neovide then require('neovide') end
