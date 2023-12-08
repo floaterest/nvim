3af125597244e1afad54f38b088d64c7fa45568c
floaterest <floaterest@users.noreply.github.com>
2023-12-08 17:57:48 -0500

    feat(neovide) scroll animation

--- lua/neovide.lua
+++ lua/neovide.lua
@@ -2,3 +2,4 @@ vim.o.gfn = 'Iosevka Custom NF,UD デジタル 教科書体 N-R:h14'
 
 vim.g.neovide_remember_window_size = true
 vim.g.neovide_cursor_animation_length = 0
+vim.g.neovide_scroll_animation_length = 0.25
