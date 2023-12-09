d806f68a18ff210426ac7b13f21de8fe5b282494
floaterest <floaterest@users.noreply.github.com>
2023-12-09 16:45:10 -0500

    feat(neovide) change scale factor

--- lua/neovide.lua
+++ lua/neovide.lua
@@ -3,3 +3,15 @@ vim.o.gfn = 'Iosevka Custom NF,UD デジタル 教科書体 N-R:h14'
 vim.g.neovide_remember_window_size = true
 vim.g.neovide_cursor_animation_length = 0
 vim.g.neovide_scroll_animation_length = 0.25
+
+
+vim.g.neovide_scale_factor = 1.3
+local change_scale_factor = function(delta)
+    vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
+end
+vim.keymap.set('n', '<C-=>', function()
+    change_scale_factor(1.25)
+end)
+vim.keymap.set('n', '<C-->', function()
+    change_scale_factor(1/1.25)
+end)
