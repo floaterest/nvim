7a69f6310a1c138fb3b07db6d95e9df0534e0426
floaterest <floaterest@users.noreply.github.com>
2024-07-19 15:39:40 -0400

    highlights.scm highlights.scm highlights.scm highl...

---
+++ after/queries/lua/highlights.scm
@@ -0,0 +1,15 @@
+;; extends 
+
+(("else" @conditional) (#set! conceal "e"))
+(("elseif" @conditional) (#set! conceal "e"))
+(("end" @keyword.function) (#set! conceal "E"))
+(("for" @repeat) (#set! conceal "F"))
+(("function" @keyword.function) (#set! conceal "f"))
+((function_call name: (identifier) @function.builtin (#eq? @function.builtin "require")) (#set! conceal "r"))
+(("in" @keyword) (#set! conceal "i"))
+(("local" @keyword) (#set! conceal "l"))
+(("do" @repeat) (#set! conceal "d"))
+(("if" @conditional) (#set! conceal "?"))
+(("and" @keyword.function) (#set! conceal "&"))
+(("return" @keyword.function) (#set! conceal "R"))
+(("then" @conditional) (#set! conceal "t"))
