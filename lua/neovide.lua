2a520f2159620dddd56bb7cdc6d5bf5f40de0e2a
floaterest <floaterest@users.noreply.github.com>
2024-07-23 12:53:29 -0400

    lazy-lock.json neovide.lua noice.lua

--- lua/neovide.lua
+++ lua/neovide.lua
@@ -3,13 +3,13 @@ if not vim.g.neovide then
 end
 
 local partial = require('plenary.functional').partial
-vim.o.gfn = 'Iosevka Custom NF,UD デジタル 教科書体 N-R:h14'
+vim.o.gfn = 'Iosevka Custom NF,UD デジタル 教科書体 N-R:h20'
 
 vim.g.neovide_remember_window_size = true
 vim.g.neovide_cursor_animation_length = 0
 vim.g.neovide_scroll_animation_length = 0.25
 
-vim.g.neovide_scale_factor = 1.3
+vim.g.neovide_scale_factor = 1
 local scale = function(delta)
     vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
 end
